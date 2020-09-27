253b84338dc10e2b877f16d3e5b6ec8a243bd06b
asahapde <60831459+asahapde@users.noreply.github.com>
2020-09-27 10:31:07 -0400

    final-commit

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ let desElement = document.getElementById("feature-description");
 
 
 // The function
-homeButton.onclick = function(){
-    imageElement.src = "images/signin.png";
-    desElement.innerHTML = "Various information about how the user is benefitting the community will be displayed.";
+homeButton.onclick = function () {
+    imageElement.src = "images/home.png";
+    desElement.innerHTML = "Various information about how the user is benefitting the community will be displayed. The user will be shown the number of community gardens near them, how many gardens they support and the average plants in the area";
 };
 
-mapButton.onclick = function(){
-    imageElement.src = "images/signin.png";
-    desElement.innerHTML = "A map of nearby community garden will be displayed.";
+mapButton.onclick = function () {
+    imageElement.src = "images/maps.png";
+    desElement.innerHTML = "A map of nearby community gardens will be displayed. The rating of the garden starting from the highest will also be displayed. The user can select if they want to support a garden from this page.";
 };
 
-newsButton.onclick = function(){
+newsButton.onclick = function () {
     imageElement.src = "images/news.png";
-    desElement.innerHTML = "News relevant to community garden will be displayed.";
+    desElement.innerHTML = "News relevant to community garden will be displayed. The user will be able to read the news starting from the most popular headline regarding community gardens.";
 };
